67ffec791faabab690120f92f71f4698cccc50ee
shashankg <shashank6511@gmail.com>
2023-09-24 02:37:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
+const path = require('path')
 //prod
+app.use(express.static{"./client/buid"});
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
